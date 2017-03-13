521c3e49b27a26fac45bf469dc883966d6c1eee3
Per Unneberg <per.unneberg@scilifelab.se>
2017-03-13 10:26:17 +0100

    Add helpers test; return (multiple) targets as list

---
+++ tests/test_helpers.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_helpers
+----------------------------------
+
+Tests for `pytest_ngsfixtures.helpers` module.
+"""
+import os
+import pytest
+from pytest_ngsfixtures import helpers, config
+
+
+class MockRule(object):
+    def __init__(self, name, output):
+        self._name = name
+        self._output = output
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def output(self):
+        return self._output
+
+
+
+def test_make_targets():
+    rules = [MockRule(name="samtools_depth", output={"txt": "{version}/{end}/medium.depth.txt"}),
+             MockRule(name="samtools_rmdup", output={"txt": "{version}/{end}/medium.rmdup.txt"})]
+    conf = config.application_config()
+    tgt = helpers.make_targets(rules, conf, 'samtools', end='se')
+    # samtools_rmdup only available for version 1.3.1
+    assert len(tgt) == 3
