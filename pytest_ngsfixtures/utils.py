e80677112d77df13df65265bb892f94791acf8dc
Per Unneberg <per.unneberg@scilifelab.se>
2017-03-15 08:30:27 +0100

    Add dry_run logging statements

---
+++ pytest_ngsfixtures/utils.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import contextlib
+
+# context manager for cd
+@contextlib.contextmanager
+def cd(path):
+    CWD = os.getcwd()
+    print("Changing directory from {} to {}".format(CWD, path), file=sys.stderr)
+
+    os.chdir(path)
+    try:
+        yield
+    except:
+        print ('Exception caught: ',sys.exc_info()[0], file=sys.stderr)
+    finally:
+        print("Changing directory back to {}".format(CWD), file=sys.stderr)
+        os.chdir(CWD)
+
