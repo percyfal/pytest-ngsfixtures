f9540e931a41d84b4a15dfa6d2b7c2cdd4d27cc0
Per Unneberg <per.unneberg@scilifelab.se>
2017-04-28 10:48:34 +0200

    Add function that maps from filetype to fixture

--- pytest_ngsfixtures/filetypes.py
+++ pytest_ngsfixtures/filetypes.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+import yaml
 from os.path import join, dirname, realpath
 from pytest_ngsfixtures import factories
 
-DATADIR = realpath(join(dirname(__file__), "data", "ref"))
+REFDIR = realpath(join(dirname(__file__), "data", "ref"))
 DATADIR = realpath(join(dirname(__file__), "data", "applications"))
 
 bam = factories.filetype(join(DATADIR, "PUR.HG00731.bam"))
@@ -15,5 +16,28 @@ gtf = ref_transcripts_gtf
 
 
 filetypes = ['bam', 'bed12', 'ref_transcripts_bed12', 'genePred', 'ref_transcripts_genePred', 'gtf', 'ref_transcripts_gtf']
+
+
+CONFIGFILE = realpath(join(DATADIR, "config.yaml"))
+with open(CONFIGFILE, "r") as fh:
+    MAPPING = yaml.load(fh)
+
+def filetype_mapping(ft, end="pe"):
+    """Retrieve mapping for filetype to fixture file name.
+
+    Params:
+      ft (str): filetype
+      end (str): sequencing mode
+
+    Returns:
+      str: fixture file names
+    """
+    if ft is None:
+        return None
+    try:
+        fn = join(DATADIR, end, MAPPING['input'][ft])
+    except Exception as e:
+        raise e
+    return fn
 
 __all__ = filetypes + ['filetypes']
