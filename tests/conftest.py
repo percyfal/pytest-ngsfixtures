567440f35fb545745f1fd4cb7b7dac26579b6143
Per Unneberg <per.unneberg@scilifelab.se>
2017-11-17 10:11:34 +0100

    Add tiny_fastq_files fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+import os
+import pytest
+from pytest_ngsfixtures import DATA_DIR
+
 pytest_plugins = 'pytester'
+
+
+@pytest.fixture(scope="module", autouse=False)
+def tiny_fastq_files():
+    """Fixture that returns list of all tiny fastq files"""
+    for path, dirs, files in os.walk(os.path.join(DATA_DIR, "tiny")):
+        filelist = sorted([os.path.join(DATA_DIR, "tiny", x) for x in files])
+    return filelist
