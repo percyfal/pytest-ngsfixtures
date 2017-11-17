9a085c6bb0bb38c2f028e1adcee8d52590b5c203
Per Unneberg <per.unneberg@scilifelab.se>
2017-11-17 11:35:47 +0100

    Improve config names for functions returning fixture metadata
    Fixes #29

--- pytest_ngsfixtures/config.py
+++ pytest_ngsfixtures/config.py
@@ -5,15 +5,19 @@ import yaml
 import itertools
 import logging
 from collections import namedtuple
-from pytest_ngsfixtures import ROOT_DIR, helpers
+from pytest_ngsfixtures import DATA_DIR, helpers
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-DATADIR = os.path.join(ROOT_DIR, "data", "applications")
+APPLICATION_DATA_DIR = os.path.join(DATA_DIR, "applications")
 APPLICATION_BLACKLIST = ["pe", "se"]
-APPLICATION_DIRECTORIES = sorted([os.path.join(DATADIR, x) for x in os.listdir(DATADIR) if os.path.isdir(os.path.join(DATADIR, x)) and x not in APPLICATION_BLACKLIST])
-configfile = os.path.join(DATADIR, "config.yaml")
+APPLICATION_DIRECTORIES = sorted(
+    [os.path.join(APPLICATION_DATA_DIR, x) for x in os.listdir(APPLICATION_DATA_DIR) if
+     os.path.isdir(os.path.join(APPLICATION_DATA_DIR, x)) and x not in
+     APPLICATION_BLACKLIST]
+)
+configfile = os.path.join(APPLICATION_DATA_DIR, "config.yaml")
 
 Config = namedtuple('Config', 'SIZES SAMPLES POPULATIONS SAMPLE_LAYOUTS')
 
@@ -32,13 +36,38 @@ sample_conf = Config(
 
 
 def application_config(application=None):
-    """Get application configuration
+    """Get application configuration.
 
-    Params:
+    Application output files are stored in the "data/applications"
+    subdirectory of pytest-ngsfixtures installation directory. Every
+    application lives in its own subdirectory named after the
+    application.
+
+    The application configuration is a dictionary that merges a
+    top-level configuration file 'config.yaml' with
+    application-specific 'config.yaml' files residing in the
+    application directories. The general configuration defines keys
+    'basedir', 'end', 'input', and 'params'. The application
+    configuration sets metadata used by snakemake to auto-generate
+    application data output.
+
+    Args:
       application (str): application name
 
     Return:
       dict: application configuration
+
+    Example:
+
+      Retrieve application configuration and use it to generate a
+      fixture. See also
+      :mod:`~pytest_ngsfixtures.factories.application_output`.
+
+      >>> from pytest_ngsfixtures import config
+      >>> conf = config.application_config()
+      >>> # Application rule metadata are formatted as python miniformat strings
+      >>> conf['qualimap']['qualimap_bamqc_pe']['output']['genome_results']
+      '{version}/{end}/genome_results.txt'
 
     """
     with open(configfile, 'r') as fh:
@@ -58,17 +87,34 @@ def application_config(application=None):
     return application_config
 
 
-def get_application_fixture(application, command, version, end="se"):
-    """Retrieve a application fixture as formatted strings
+def get_application_fixture_output(application, command, version, end="se"):
+    """Retrieve application output names as mapping from snakemake rule
+    keyword to output file name.
 
-    Params:
+    An application configuration file defines snakemake rule metadata
+    such as output. The output section simply maps a rule keyword to
+    an output file name. This function returns a key-value mapping
+    from keyword to *formatted* output file name.
+
+    Args:
       application (str): application name
       command (str): command name
       version (str): version
       end (str): se or pe
 
     Returns:
-      dict: dictionary of application fixture names formatted as a string
+      dict: dictionary that maps snakemake rule keywords to formatted
+            application output strings
+
+    Example:
+    
+      >>> from pytest_ngsfixtures import config
+      >>> af = config.get_application_fixture_output('qualimap', 'qualimap_bamqc_pe', '2.2.2')
+      >>> print(list(af.keys())[0:2])
+      ['genome_results', 'coverage_across_reference']
+      >>> print(af['genome_results'])
+      qualimap/2.2.2/se/genome_results.txt
+
     """
     conf = application_config(application)
     try:
@@ -79,20 +125,38 @@ def get_application_fixture(application, command, version, end="se"):
     return {k: os.path.join(application, o.format(version=version, end=end)) for k, o in output.items()}
 
 
-def application_fixtures(application=None, end=None, version=None):
-    """Return the application fixtures.
+def flattened_application_fixture_metadata(application=None, end=None, version=None):
+    """Return the application fixture metadata flattened as a list of tuples.
 
-    Returns the application fixtures defined in the application config
-    file (data/applications/config.yaml).
+    Returns the application fixture metadata defined in the
+    application config file (data/applications/config.yaml) as a list
+    of tuples.
 
-    Params:
+    Args:
       application (str): application name
       end (str): sequence configuration (single end/paired end)
       version (str): version identifier
 
     Returns:
-      list of fixtures, where each entry consists of application,
-      command, version, end, and the raw output.
+      flattened list of fixture metadata, where each entry consists of
+      application, command, version, end, and the raw unformatted output.
+
+    Example:
+
+      Typically this function is used to collect all metadata needed
+      to setup a (parametrized) pytest fixture. See
+      :py:mod:`bioodo.tests.utils.fixture_factory` for an example
+      where a list of fixture metadata is used to generate a set of
+      parametrized fixtures.
+
+      >>> from pytest_ngsfixtures import config
+      >>> af = config.flattened_application_fixture_metadata(application="qualimap")
+      >>> module, command, version, end, fmtdict = af[0]
+      >>> module, command, version, end
+      ('qualimap', 'qualimap_bamqc_pe', '2.2.2', 'pe')
+      >>> fmtdict['genome_results']
+      '{version}/{end}/genome_results.txt'
+
     """
     fixtures = []
     conf = application_config(application)
