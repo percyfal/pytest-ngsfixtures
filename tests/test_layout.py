427fc267dffc580f62229945a30ad935dc211465
Per Unneberg <per.unneberg@scilifelab.se>
2017-11-20 21:49:38 +0100

    Add tests for file and layout

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -10,7 +10,7 @@ import pytest
 import py
 from pytest_ngsfixtures.os import safe_mktemp
 from pytest_ngsfixtures.file import ReadFixtureFile
-from pytest_ngsfixtures.layout import generate_sample_layouts, sample_fixture_layout
+from pytest_ngsfixtures.layout import generate_sample_layouts, sample_fixture_layout, reference_fixture_layout
 
 
 def test_generate_sample_layouts():
@@ -57,7 +57,31 @@ def test_sample_fixture_layout_flat_sample_copy(tmpdir_factory):
 
 def test_sample_fixture_layout_individual(tmpdir_factory):
     p = safe_mktemp(tmpdir_factory)
-    p = sample_fixture_layout(p, layout="individual")
+    p = sample_fixture_layout(p, layout="individual", sampleinfo=True)
     flist = [x.basename for x in p.visit() if str(x).endswith(".fastq.gz")]
     assert "PUR.HG00731_010101_AAABBB11XX_1.fastq.gz" in flist
     assert "PUR.HG00731_020202_AAABBB22XX_1.fastq.gz" in flist
+    header = p.join("sampleinfo.csv").readlines()[0].strip()
+    assert header == "PU,SM,fastq"
+
+
+def test_reference_fixture_layout(tmpdir):
+    path = reference_fixture_layout(tmpdir.join("ref"))
+    flist = [x.basename for x in path.visit()]
+    assert "ref.fa" in flist
+    assert "scaffolds.fa" not in flist
+
+
+def test_sample_fixture_layout_individual_pop(tmpdir):
+    path = sample_fixture_layout(tmpdir, layout="individual",
+                                 sampleinfo=True, runfmt="{POP}/{BATCH}/{PU}/{SM}")
+    flist = [x.basename for x in path.visit() if str(x).endswith(".fastq.gz")]
+    assert "PUR.HG00731_1.fastq.gz" in flist
+    assert "PUR.HG00731_1.fastq.gz" in flist
+    sampleinfo = sorted(path.join("sampleinfo.csv").readlines())
+    header = sampleinfo[0].strip()
+    assert header == "BATCH,POP,PU,SM,fastq"
+    chs = sampleinfo[2].strip().split(",")
+    assert chs[0] == "p1"
+    assert chs[1] == "CHS"
+    assert chs[4] == "CHS/p1/010101_AAABBB11XX/CHS.HG00512_2.fastq.gz"
