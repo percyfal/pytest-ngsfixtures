2f7a3a2b2f1095782b5717f523bc17c497b4d85c
Per Unneberg <per.unneberg@scilifelab.se>
2017-11-21 10:38:13 +0100

    Fix some failing layout configs and tests

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -8,9 +8,8 @@ Tests for `pytest_ngsfixtures.layout` module.
 """
 import pytest
 import py
-from pytest_ngsfixtures.os import safe_mktemp
 from pytest_ngsfixtures.file import ReadFixtureFile
-from pytest_ngsfixtures.layout import generate_sample_layouts, sample_fixture_layout, reference_fixture_layout
+from pytest_ngsfixtures.layout import generate_sample_layouts, setup_sample_layout, setup_reference_layout
 
 
 def test_generate_sample_layouts():
@@ -22,11 +21,10 @@ def test_generate_sample_layouts():
 
 def test_filefixtures_from_sample_layouts(tmpdir):
     layout = generate_sample_layouts()
-    p = tmpdir.mkdir("layout")
     for l in layout:
-        r1 = ReadFixtureFile(runfmt="{SM}_{PU}", path=p, **l)
+        r1 = ReadFixtureFile(runfmt="{SM}_{PU}", path=tmpdir, **l)
         r1.setup()
-        r2 = ReadFixtureFile(runfmt="{SM}_{PU}", path=p, read=2,
+        r2 = ReadFixtureFile(runfmt="{SM}_{PU}", path=tmpdir, read=2,
                              copy=True, **l)
         r2.setup()
         assert r1.islink()
@@ -36,28 +34,24 @@ def test_filefixtures_from_sample_layouts(tmpdir):
         assert r2.src.exists()
 
 
-def test_sample_fixture_layout_flat(tmpdir_factory):
-    p = safe_mktemp(tmpdir_factory)
-    p = sample_fixture_layout(p)
+def test_setup_sample_layout_flat(tmpdir):
+    p = setup_sample_layout(tmpdir, layout="short", runfmt="{SM}_{PU}")
     flist = [str(x.basename) for x in p.visit() if str(x).endswith(".fastq.gz")]
     assert 's1_010101_AAABBB11XX_2.fastq.gz' in flist
 
 
-def test_sample_fixture_layout_flat_sample(tmpdir_factory):
-    p = safe_mktemp(tmpdir_factory)
+def test_setup_sample_layout_flat_sample(tmpdir):
     with pytest.raises(py.error.EEXIST):
-        sample_fixture_layout(p, runfmt="{SM}")
+        setup_sample_layout(tmpdir, layout="short", runfmt="{SM}")
 
 
-def test_sample_fixture_layout_flat_sample_copy(tmpdir_factory):
-    p = safe_mktemp(tmpdir_factory)
+def test_setup_sample_layout_flat_sample_copy(tmpdir):
     with pytest.raises(py.error.EEXIST):
-        sample_fixture_layout(p, runfmt="{SM}", copy=True)
+        setup_sample_layout(tmpdir, layout="short", runfmt="{SM}", copy=True)
 
 
-def test_sample_fixture_layout_individual(tmpdir_factory):
-    p = safe_mktemp(tmpdir_factory)
-    p = sample_fixture_layout(p, layout="individual", sampleinfo=True)
+def test_setup_sample_layout_individual(tmpdir):
+    p = setup_sample_layout(tmpdir, layout="individual", sampleinfo=True)
     flist = [x.basename for x in p.visit() if str(x).endswith(".fastq.gz")]
     assert "PUR.HG00731_010101_AAABBB11XX_1.fastq.gz" in flist
     assert "PUR.HG00731_020202_AAABBB22XX_1.fastq.gz" in flist
@@ -65,16 +59,16 @@ def test_sample_fixture_layout_individual(tmpdir_factory):
     assert header == "PU,SM,fastq"
 
 
-def test_reference_fixture_layout(tmpdir):
-    path = reference_fixture_layout(tmpdir.join("ref"))
+def test_setup_reference_layout(tmpdir):
+    path = setup_reference_layout(tmpdir.join("ref"))
     flist = [x.basename for x in path.visit()]
     assert "ref.fa" in flist
     assert "scaffolds.fa" not in flist
 
 
-def test_sample_fixture_layout_individual_pop(tmpdir):
-    path = sample_fixture_layout(tmpdir, layout="individual",
-                                 sampleinfo=True, runfmt="{POP}/{BATCH}/{PU}/{SM}")
+def test_setup_sample_layout_individual_pop(tmpdir):
+    path = setup_sample_layout(tmpdir, layout="individual",
+                               sampleinfo=True, runfmt="{POP}/{BATCH}/{PU}/{SM}")
     flist = [x.basename for x in path.visit() if str(x).endswith(".fastq.gz")]
     assert "PUR.HG00731_1.fastq.gz" in flist
     assert "PUR.HG00731_1.fastq.gz" in flist
