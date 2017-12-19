7ecfbec4a33371d9abc79e6886d7d4d608decac3
Per Unneberg <per.unneberg@scilifelab.se>
2017-12-19 12:11:13 +0100

    Remove unused testdir namespace

--- pytest_ngsfixtures/tests/conftest.py
+++ pytest_ngsfixtures/tests/conftest.py
@@ -1,11 +1,2 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
-import py
-
-
-def pytest_namespace():
-    d = {
-        'testdir': py.path.local(os.path.abspath(os.path.dirname(__file__))),
-    }
-    return d
