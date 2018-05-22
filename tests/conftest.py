87a046d97be3db04c68f4f693c0683f5bfe47e0e
Per Unneberg <per.unneberg@scilifelab.se>
2018-05-22 17:52:11 +0200

    Remove obsolete code
    Remove majority of code base to focus on simplicity

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,19 +5,15 @@ import py
 import pytest
 import docker
 import subprocess as sp
-from pytest_ngsfixtures import DATA_DIR, factories
+from pytest_ngsfixtures import DATA_DIR
 from pytest_ngsfixtures.os import localpath
-from pytest_ngsfixtures.fixtures import *
 import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-logging.getLogger("urllib3").setLevel(logging.INFO)
-logging.getLogger("requests").setLevel(logging.INFO)
 logging.getLogger("docker").setLevel(logging.INFO)
 
 pytest_plugins = 'pytester'
-
 
 PYTHON_VERSION = "py{}{}".format(sys.version_info.major,
                                  sys.version_info.minor)
@@ -156,65 +152,3 @@ def image_args():
         'tty': False,
     }
     return d
-
-
-@pytest.fixture(scope="module", autouse=False)
-def tiny_fastq_files():
-    """Fixture that returns list of all tiny fastq files"""
-    for path, dirs, files in os.walk(os.path.join(DATA_DIR, "tiny")):
-        filelist = sorted([os.path.join(DATA_DIR, "tiny", x) for x in files])
-    return filelist
-
-
-# Local files
-PURHG00731 = localpath(os.path.join("applications", "pe",
-                                    "PUR.HG00731.tiny.bam"))
-PURHG00733 = localpath(os.path.join("applications", "pe",
-                                    "PUR.HG00733.tiny.bam"))
-
-
-# File fixtures
-@pytest.fixture(scope="function", autouse=False)
-def bamfile():
-    """Bamfile fixture"""
-    return PURHG00731
-
-
-bam = factories.filetype(PURHG00731.basename,
-                         fdir="bamfoo", scope="function",
-                         numbered=True)
-bam_copy = factories.filetype(PURHG00731.basename, fdir="bamfoo",
-                              scope="function", numbered=True,
-                              copy=True)
-renamebam = factories.filetype(PURHG00731.basename,
-                               fdir="renamebamfoo", scope="function",
-                               numbered=True, alias="s.tiny.bam")
-renamebam_copy = factories.filetype(PURHG00731.basename, fdir="renamebamfoo",
-                                    scope="function", numbered=True,
-                                    alias="s.tiny.bam", copy=True)
-
-
-# Multifile fixtures
-_PURFILES = [PURHG00731.relto(DATA_DIR), PURHG00733.relto(DATA_DIR)]
-
-
-@pytest.fixture(scope="function", autouse=False)
-def PURFILES():
-    """List of bam files"""
-    return [PURHG00731, PURHG00733]
-
-
-bamset = factories.fileset(src=_PURFILES, fdir="bamset",
-                           scope="function")
-
-_dstfiles = ["foo.fastq.gz", "bar.fastq.gz"]
-
-
-@pytest.fixture(scope="function", autouse=False)
-def dstfiles():
-    """Destination file names"""
-    return _dstfiles
-
-
-bamset2 = factories.fileset(src=_PURFILES, dst=_dstfiles,
-                            fdir="bamset2", scope="function")
