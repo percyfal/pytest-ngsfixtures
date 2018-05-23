c4440361c4dcfdf8114277490e54cc473919469d
Per Unneberg <per.unneberg@scilifelab.se>
2018-05-23 12:55:47 +0200

    Delete obsolete modules and tests

--- pytest_ngsfixtures/factories.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import logging
-import itertools
-import pytest
-from pytest_ngsfixtures.os import safe_mktemp
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
